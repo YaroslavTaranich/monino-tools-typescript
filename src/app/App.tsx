92cf3fb7354ac69c9a4b94ac6983e13fefe08cee
Yaroslav Taranich <YaroslavTaranich@yandex.ru>
2022-12-01 18:04:03 +0300

    fix app

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,10 @@
-import React, { useEffect } from 'react'
+import { useEffect } from 'react'
 
 import { useAppDispatch, useAppSelector } from '../redux/hooks'
 import { getAllCategory, selectCategoryStatus } from '../redux/slices/categorySlice'
 import { getAllTools, selectToolsStatus } from '../redux/slices/toolsSlice'
 import Spinner from '../components/UI/spinner/Spinner'
 import AppRouter from '../router/AppRouter'
-// import HidebleSliderButton from '../components/UI/spinner/HidebleSliderButton/HidebleSliderButton'
 import Header from '../components/Header/Header'
 
 // import styles from './App.module.scss'
@@ -23,6 +22,7 @@ function App() {
   if (toolsStatus.fetchStatus === 'pending' || categoryStatus.fetchStatus === 'pending') {
     return <Spinner />
   }
+
   return (
     <>
       <Header />
