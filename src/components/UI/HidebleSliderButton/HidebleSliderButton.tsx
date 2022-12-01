02a45615ec0c0ee844fe3e401ebd8bc3a28b4bce
Yaroslav Taranich <YaroslavTaranich@yandex.ru>
2022-12-01 15:59:50 +0300

    add use mount to header buttons

--- src/components/UI/HidebleSliderButton/HidebleSliderButton.tsx
+++ src/components/UI/HidebleSliderButton/HidebleSliderButton.tsx
@@ -3,6 +3,7 @@ import { faClose, IconDefinition } from '@fortawesome/free-solid-svg-icons'
 import { FC, ReactElement, useEffect, useState } from 'react'
 
 import ButtonIconRound from '../BottonIconRound/ButtonIconRound'
+import { useMount } from '../../../hooks/useMount'
 
 import styles from './HidebleSliderButton.module.scss'
 
@@ -10,15 +11,25 @@ interface HidebleSliderButtonProps {
   icon: IconDefinition
   children: ReactElement | ReactElement[]
   transitionX?: number
+  onClose?: () => void
 }
 
-const HidebleSliderButton: FC<HidebleSliderButtonProps> = ({ icon, children, transitionX }) => {
+const HidebleSliderButton: FC<HidebleSliderButtonProps> = ({ icon, children, transitionX, onClose }) => {
   const [open, setOpen] = useState(false)
+
+  const mount = useMount(open, 400)
 
   const keyHandler = (e: KeyboardEvent) => {
     if (e.key === 'Escape') {
       setOpen(false)
     }
+  }
+
+  const buttonHandler = () => {
+    setOpen((prev) => {
+      if (prev) onClose!()
+      return !prev
+    })
   }
 
   useEffect(() => {
@@ -39,8 +50,8 @@ const HidebleSliderButton: FC<HidebleSliderButtonProps> = ({ icon, children, tra
           : { transform: `translate(-${transitionX}rem, -50%)`, zIndex: 0 }
       }
     >
-      <div className={styles.child}>{children}</div>
-      <ButtonIconRound onClick={() => setOpen((prev) => !prev)}>
+      <div className={styles.child}>{mount ? children : null}</div>
+      <ButtonIconRound onClick={buttonHandler}>
         <FontAwesomeIcon icon={open ? faClose : icon} />
       </ButtonIconRound>
     </div>
@@ -49,6 +60,7 @@ const HidebleSliderButton: FC<HidebleSliderButtonProps> = ({ icon, children, tra
 
 HidebleSliderButton.defaultProps = {
   transitionX: 0,
+  onClose: () => {},
 }
 
 export default HidebleSliderButton
