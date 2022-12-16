bfda4213a3f4c9adcf6d6a0a0aaede754b1ca123
Yaroslav Taranich <YaroslavTaranich@yandex.ru>
2022-12-16 13:53:21 +0300

    update page title component

--- src/components/UI/PageTitle/PageTitle.tsx
+++ src/components/UI/PageTitle/PageTitle.tsx
@@ -1,11 +1,22 @@
+import { HTMLAttributes } from 'react'
+
 import styles from './PageTitle.module.scss'
 
-interface PageTitleProps {
+interface PageTitleProps extends HTMLAttributes<HTMLHeadingElement> {
   children: string
+  variant?: 'primary' | 'secondary'
 }
 
-function PageTitle({ children }: PageTitleProps) {
-  return <h1 className={styles.title}>{children}</h1>
+function PageTitle({ children, variant, ...props }: PageTitleProps) {
+  return (
+    <h1 className={styles[variant!]} {...props}>
+      {children}
+    </h1>
+  )
+}
+
+PageTitle.defaultProps = {
+  variant: 'primary',
 }
 
 export default PageTitle
